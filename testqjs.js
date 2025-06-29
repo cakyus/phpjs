715189c315e4a456037fc4a6f2979db07608eef0
cakyus <cakyus@gmail.com>
2025-06-29 16:53:43 +0800

    use separate test from qjs and tjs

---
+++ testqjs.js
@@ -0,0 +1,6 @@
+import { empty } from './libphpqjs.js';
+
+if (empty(0) == false) {
+  console.log('ERROR: empty number zero.');
+}
+
---
+++ testtjs.js
@@ -0,0 +1,6 @@
+import { empty } from './libphptjs.js';
+
+if (empty(0) == false) {
+  console.log('ERROR: empty number zero.');
+}
+

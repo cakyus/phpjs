40564fe4dc60e9a84eecc2a8901e9ff826620947
cakyus <cakyus@gmail.com>
2025-07-06 08:10:09 +0800

    add assert()

--- test.js
+++ test.js
@@ -1,6 +1,38 @@
-import { empty } from './libphp.js';
+import {
+  empty
+} from './libphp.js';
 
-if (empty(0) == false) {
-  console.log('ERROR: empty number zero.');
+assert(true, empty(0));
+assert(true, empty(''));
+
+function assert(expect, result, message) {
+
+  let expect_type = typeof(expect);
+  let result_type = typeof(result);
+
+  if (expect_type != result_type) {
+    throw Error('expect '+ expect_type + ' result ' + result_type + '.');
+  } else if (expect_type == 'boolean') {
+    if (expect == result) {
+      // do nothing
+    } else if (expect == true) {
+      throw Error('expect false result true.');
+    } else {
+      throw Error('expect true result false.');
+    }
+  } else if (expect_type == 'string') {
+    if (expect == result) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (expect_type == 'number') {
+    if (expect == result) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    throw Error('Unknown type. ' + type);
+  }
 }
-

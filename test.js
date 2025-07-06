84f9321fae1babf39d370ec4342db684b8ebc0b2
cakyus <cakyus@gmail.com>
2025-07-06 10:12:52 +0800

    move assert() to libtest.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+import { assert } from './libtest.js';
 import {
   fnmatch,
   empty
@@ -23,34 +24,3 @@ function test_empty() {
   assert(true, empty(undefined));
 }
 
-function assert(expect, result, message) {
-
-  let expect_type = typeof(expect);
-  let result_type = typeof(result);
-
-  if (expect_type != result_type) {
-    throw Error('expect '+ expect_type + ' result ' + result_type + '.');
-  } else if (expect_type == 'boolean') {
-    if (expect == result) {
-      // do nothing
-    } else if (expect == true) {
-      throw Error('expect false result true.');
-    } else {
-      throw Error('expect true result false.');
-    }
-  } else if (expect_type == 'string') {
-    if (expect == result) {
-      return true;
-    } else {
-      return false;
-    }
-  } else if (expect_type == 'number') {
-    if (expect == result) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    throw Error('Unknown type. ' + type);
-  }
-}
